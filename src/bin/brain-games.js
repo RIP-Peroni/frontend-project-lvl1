7baf4eb88ed9d02ac8cb4f5e7d7673a53339e3a8
RIP-Peroni <epih96@mail.ru>
2019-09-10 01:55:38 +0300

    babel + hello, <name>!

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,3 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!');
+import { ourFunction } from '..';
+ourFunction();
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import readlineSync from 'readline-sync';
+export const ourFunction = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}`);
+};
