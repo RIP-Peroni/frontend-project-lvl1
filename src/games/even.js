c0a7a888fef2ee7389cb03d391733c90a40ddf28
RIP-Peroni <epih96@mail.ru>
2019-09-27 03:26:53 +0300

    1.3 corrections

--- src/games/even.js
+++ src/games/even.js
@@ -3,9 +3,8 @@ import { getRandomNumber } from '../getRandomNumber';
 
 const ruleForGame = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEvenNumber = (number) => number % 2 === 0;
-
 const createLogicOfGameEven = () => {
+  const isEvenNumber = (number) => number % 2 === 0;
   const question = getRandomNumber(100);
   const correctAnswer = isEvenNumber(question) ? 'yes' : 'no';
   return {
